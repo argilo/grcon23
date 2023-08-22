c2490e7158a9d09acb65e21a20976ea6c894de6e
Clayton Smith <argilo@gmail.com>
2023-08-22 17:22:54 -0400

    Finish sigid part 4

--- sigid/gen.sh
+++ sigid/gen.sh
@@ -6,9 +6,17 @@ convert \
   -background White \
   -gravity Center \
   -pointsize 15 \
-  label:'flag\{paint_all_the_things\}' \
+  label:'Flag 1: flag\{paint_all_the_things\}' \
   -rotate 90 \
-  -extent x220 \
-  -crop 17x220-2+0 \
+  -extent x260 \
+  -crop 17x260-2+0 \
+  -flop \
   -depth 8 \
   gray:freedv_paint_flag.raw
+
+sox part4-flag2.wav --bits 16 --encoding signed-integer --endian little part4-flag2.raw
+~/git/codec2/build_linux/src/freedv_tx 1600 part4-flag2.raw freedv.s16
+cat <(dd if=/dev/zero bs=2 count=40000) freedv.s16 <(dd if=/dev/zero bs=2 count=40000) > freedv_padded.s16
+
+grcc freedv.grc
+./freedv.py
